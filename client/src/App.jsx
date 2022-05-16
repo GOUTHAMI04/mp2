78e62db9bcde25908b5526a58aac66f787feabf8
gouthami04 <gouthamigongidi2457@gmail.com>
2022-05-16 11:08:14 +0530

    firs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 
 const cookies = new Cookies();
 
-const apiKey = '9h68887uw6rf';
+const apiKey = 'vx3usy8xpd5m';
 const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
